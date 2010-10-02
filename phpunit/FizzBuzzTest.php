bd48cd167a6eac2254f311ca0fc7b8ac271bd262
sanemat <o.gata.ken@gmail.com>
2010-10-02 21:12:31 +0900

    case 2

--- phpunit/FizzBuzzTest.php
+++ phpunit/FizzBuzzTest.php
@@ -7,5 +7,10 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
     $fb = new FizzBuzz();
     $this->assertEquals(1, $fb->say(1));
   }
+  public function testTwo()
+  {
+    $fb = new FizzBuzz();
+    $this->assertEquals(2, $fb->say(2));
+  }
 }
 ?>

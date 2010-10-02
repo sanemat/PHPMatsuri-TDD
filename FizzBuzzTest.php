ab75afb3d791c4229f938c6ce0305823396b2394
sanemat <o.gata.ken@gmail.com>
2010-10-02 20:08:37 +0900

    initial red test

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,10 +1,8 @@
 <?php
-include_once 'fizzbuzz.php';
 include_once 'lime.php';
 
-$test = new lime_test(null,new lime_output_color(true));
+$t = new lime_test(null,new lime_output_color(true));
 
-$fuzzbuzz = new fizzbuzz();
-$test->is('fizzbuzz', $fuzzbuzz->fizzbuzz());
-
+$fizzBuzz = new FizzBuzz();
+$t->fail('initial fail test');
 ?>

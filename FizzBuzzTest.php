e4c344bb3b77ddf11b3a061b87983fdd0659eb73
sanemat <o.gata.ken@gmail.com>
2010-10-02 20:29:14 +0900

    add comment 1 and 2

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -4,8 +4,8 @@ require_once 'FizzBuzz.php';
 $t = new lime_test(null,new lime_output_color(true));
 
 $fizzBuzz = new FizzBuzz();
-$t->is($fizzBuzz->say(1), 1);
-$t->is($fizzBuzz->say(2), 2);
+$t->is($fizzBuzz->say(1), 1, '1のときに1を返すこと');
+$t->is($fizzBuzz->say(2), 2, '2のときに2を返すこと');
 $t->is($fizzBuzz->say(3), 'Fizz', '3のときにFizzを返すこと');
 $t->is($fizzBuzz->say(6), 'Fizz', '6のときにFizzを返すこと');
 $t->is($fizzBuzz->say(5), 'Buzz', '5のときにBuzzを返すこと');
